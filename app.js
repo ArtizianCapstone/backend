9d2396e917ad4e14c9f1e139d530546ad8f42758
Jackson Kurtz <jkurtz678@gmail.com>
2018-11-29 12:18:24 -0800

    added better response handling and schema relationship

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes= require('./api/routes/orders');
@@ -15,6 +15,8 @@ mongoose.connect(
         useNewUrlParser: true
     }
 );
+
+mongoose.Promise = global.Promise;
 
 app.use( morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
