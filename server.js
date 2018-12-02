abd21121a8ee0935511b4b5855017263a0aac919
Jackson Kurtz <jkurtz678@gmail.com>
2018-12-02 12:59:35 -0800

    changing server.js to accept requests over wifi, still doesn't work

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require('http');
 const app = require('./app');
+const publicIp = require( 'public-ip');
 
 const port = process.env.Port || 3000
 const server = http.createServer(app);
 
-server.listen(port);
+const myIp = "0.0.0.0";
+/*
+publicIp.v4().then(ip => {
+    myIp = ip;
+    console.log("your public ip address", ip );
+});*/
+
+server.listen(port, myIp);/*, myIp , function() {
+    console.log('Listening to port:  ' + port );
+});*/
